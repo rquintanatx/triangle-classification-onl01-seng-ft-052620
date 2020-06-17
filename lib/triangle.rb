67e7b25d3c3cd63ac894c2c1533847872acce331
Robert Quintana <robertquintanatx@gmail.com>
2020-06-17 01:15:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,13 +11,15 @@ class Triangle
   end 
   
   def kind 
-    if @len1 > 0 && @len2 > 0 && @len3 > 0 && (@len1 + @len2 > @len3 || @len2 + @len3 > @len1 || @len1 + @len3 > @len2)
+    if @len1 > 0 && @len2 > 0 && @len3 > 0
       if @len1 == @len2 && @len1 == @len3
         :equilateral
       elsif @len1 == @len2 || @len2 == @len3 || @len3 == @len1
         :isosceles
       elsif @len1 != @len2 && @len2 != @len3 && @len3 != @len1
         :scalene
+      else 
+        raise TriangleError
       end 
     else 
       raise TriangleError
