bcf088e393ee817593d34ed544505381d3f12bbb
Robert Quintana <robertquintanatx@gmail.com>
2020-06-17 01:18:23 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_accessor :len1, :len2, :len3
+  
+  def initialize(len1, len2, len3)
+    @len1 = len1
+    @len2 = len2 
+    @len3 = len3
+  end 
+  
+  def kind 
+    if @len1 > 0 && @len2 > 0 && @len3 > 0 &&(@len1 + @len2 > @len3 && @len1 + @len3 > @len2 && @len2 + @len3 > @len1)
+      if @len1 == @len2 && @len1 == @len3
+        :equilateral
+      elsif @len1 == @len2 || @len2 == @len3 || @len3 == @len1
+        :isosceles
+      elsif @len1 != @len2 && @len2 != @len3 && @len3 != @len1
+        :scalene
+      else 
+        raise TriangleError
+      end 
+    else 
+      raise TriangleError
+    end
+  end 
+   
+  class TriangleError < StandardError
+    
+  end 
+  
 end
